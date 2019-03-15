6d6fc422691f40cc5a4f519dae4414c5696e5671
jonolds <jonolds@gmail.com>
2019-03-15 12:49:42 -0500

    win

--- src/main/java/Node.java
+++ src/main/java/Node.java
@@ -15,41 +15,18 @@ public class Node {
 		String[] map = str.split("\t");
 		String key = map[0], value = map[1];
 		String[] tokens = value.split("\\|");
-		
-		//!!!!!!!!!!!! FIX WEIGHTS WORKAROUND
-//		if(tokens.length == 3)
-//			tokens = new String[] {tokens[0], tokens[0], tokens[1], tokens[2]};
 
-		//ID
+		//ID  -  EDGES  -  WEIGHTS  -  COST  -  COLOR
 		this.id = Integer.parseInt(key);
-		//EDGES
 		Arrays.stream(tokens[0].split(",")).filter(x->x.length()>0).forEach(x->edges.add(Integer.parseInt(x)));
-		//WEIGHTS
 		Arrays.stream(tokens[1].split(",")).filter(x->x.length()>0).forEach(x->weights.add(Integer.parseInt(x)));
-		//COST
 		this.cost = (tokens[2].equals("Integer.MAX_VALUE")) ? Integer.MAX_VALUE : Integer.parseInt(tokens[2]);
-		//COLOR
 		this.color = Color.valueOf(tokens[3]);
 	}
-
 	public Node(int id, List<Integer> edges, List<Integer> weights, int cost, Color color) {
 		this.id = id; this.edges = edges; this.weights = weights; this.cost = cost; this.color = color;
 	}
-	
 	public Node(int id) { this.id = id; weights.add(0); }
-	public int getId() { return this.id; }
-
-	public List<Integer> getEdges() { return this.edges; }
-	public void setEdges(List<Integer> edges) { this.edges = edges; }
-
-	public List<Integer> getWeights() { return this.weights; }
-	public void setWeights(List<Integer> weights) { this.weights = weights; }
-	
-	public int getCost() { return this.cost; }
-	public void setCost(int cost) { this.cost = cost; }
-
-	public Color getColor() { return this.color; }
-	public void setColor(Color color) { this.color = color; }
 	
 	public void setCostColor(int cost, Color color) { this.cost = cost; this.color = color; }
 	
@@ -60,5 +37,15 @@ public class Node {
 		s.append(this.cost < Integer.MAX_VALUE ? this.cost : "Integer.MAX_VALUE").append("|");
 		s.append(color.toString());
 		return new Text(s.toString());
-	}
+	}	
+
+	public int getId() { return this.id; }
+	public List<Integer> getEdges() { return this.edges; }
+	public void setEdges(List<Integer> edges) { this.edges = edges; }
+	public List<Integer> getWeights() { return this.weights; }
+	public void setWeights(List<Integer> weights) { this.weights = weights; }
+	public int getCost() { return this.cost; }
+	public void setCost(int cost) { this.cost = cost; }
+	public Color getColor() { return this.color; }
+	public void setColor(Color color) { this.color = color; }
 }
